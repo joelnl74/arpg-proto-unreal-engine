a16ebd41bf6493c144366de445886324959991bf
Joey <joelnl74@gmail.com>
2023-06-26 02:01:51 +0200

    setting up test values

--- Source/ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.cpp
+++ Source/ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.cpp
@@ -1,2 +1,41 @@
 #include "ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+UPlayerGameplayAttributeSet::UPlayerGameplayAttributeSet()
+{
+	Inithealth(100.f);
+	InitmaxHealth(100.f);
+	Initmana(50.f);
+	InitmaxMana(50.f);
+}
+
+void UPlayerGameplayAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerGameplayAttributeSet, health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerGameplayAttributeSet, maxHealth, COND_None, REPNOTIFY_Always);
+
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerGameplayAttributeSet, mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UPlayerGameplayAttributeSet, maxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UPlayerGameplayAttributeSet::onRep_Health(const FGameplayAttributeData& oldHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerGameplayAttributeSet, health, oldHealth);
+}
+
+void UPlayerGameplayAttributeSet::onRep_MaxHealth(const FGameplayAttributeData& oldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerGameplayAttributeSet, maxHealth, oldMaxHealth);
+
+}
+
+void UPlayerGameplayAttributeSet::onRep_Mana(const FGameplayAttributeData& oldMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerGameplayAttributeSet, mana, oldMana);
+
+}
+
+void UPlayerGameplayAttributeSet::onRep_MaxMana(const FGameplayAttributeData& oldMaxMana)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UPlayerGameplayAttributeSet, maxMana, oldMaxMana);
+}
--- Source/ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.h
+++ Source/ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.h
@@ -4,14 +4,53 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "PlayerGameplayAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
+
 UCLASS()
 class ARPG_PROTO_API UPlayerGameplayAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UPlayerGameplayAttributeSet();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UFUNCTION()
+	void onRep_Health(const FGameplayAttributeData &oldHealth);
+
+	UFUNCTION()
+	void onRep_MaxHealth(const FGameplayAttributeData &oldMaxHealth);
+
+	UFUNCTION()
+	void onRep_Mana(const FGameplayAttributeData& oldMana);
+
+	UFUNCTION()
+	void onRep_MaxMana(const FGameplayAttributeData &oldMaxMana);
+
+public:
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = onRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData health;
+	ATTRIBUTE_ACCESSORS(UPlayerGameplayAttributeSet, health);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = onRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData maxHealth;
+	ATTRIBUTE_ACCESSORS(UPlayerGameplayAttributeSet, maxHealth);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = onRep_Mana, Category = "Vital Attributes")
+	FGameplayAttributeData mana;
+	ATTRIBUTE_ACCESSORS(UPlayerGameplayAttributeSet, mana);
+
+	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = onRep_MaxMana, Category = "Vital Attributes")
+	FGameplayAttributeData maxMana;
+	ATTRIBUTE_ACCESSORS(UPlayerGameplayAttributeSet, maxMana);
+
 };
