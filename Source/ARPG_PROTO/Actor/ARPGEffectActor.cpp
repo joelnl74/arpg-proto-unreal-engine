26112c8b6cfbdabab92c34fab0a25254b8451df6
Joey <joelnl74@gmail.com>
2023-07-04 02:01:04 +0200

    refactor gameplay effect

--- Source/ARPG_PROTO/Actor/ARPGEffectActor.cpp
+++ Source/ARPG_PROTO/Actor/ARPGEffectActor.cpp
@@ -1,50 +1,37 @@
 
 #include "ARPG_PROTO/Actor/ARPGEffectActor.h"
-
-#include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
 #include "ARPG_PROTO/AbilitySystem/PlayerGameplayAttributeSet.h"
-#include "Components/SphereComponent.h"
+
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
 
 // Sets default values
 AARPGEffectActor::AARPGEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
-	mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(mesh);
-
-	sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	sphere->SetupAttachment(GetRootComponent());
-
-}
-
-void AARPGEffectActor::OnOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult)
-{
-	//TODO: Change this to apply a Gameplay Effect. For now, using const_cast as a hack!
-	if (IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(otherActor))
-	{
-		const UPlayerGameplayAttributeSet* attributeSet = Cast<UPlayerGameplayAttributeSet>(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(UPlayerGameplayAttributeSet::StaticClass()));
-
-		UPlayerGameplayAttributeSet* mutableAuraAttributeSet = const_cast<UPlayerGameplayAttributeSet*>(attributeSet);
-		mutableAuraAttributeSet->Sethealth(attributeSet->Gethealth() + 25.f);
-		mutableAuraAttributeSet->Setmana(attributeSet->Getmana() - 25.f);
-		Destroy();
-	}
-}
-
-void AARPGEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
 // Called when the game starts or when spawned
 void AARPGEffectActor::BeginPlay()
 {
-	Super::BeginPlay();
+	Super::BeginPlay();	
+}
 
-	sphere->OnComponentBeginOverlap.AddDynamic(this, &AARPGEffectActor::OnOverlap);
-	sphere->OnComponentEndOverlap.AddDynamic(this, &AARPGEffectActor::EndOverlap);
-	
+void AARPGEffectActor::ApplyEffectToTarget(AActor* target, TSubclassOf<UGameplayEffect> gameplayEffectClass)
+{
+	UAbilitySystemComponent* targetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(target);
+	if (targetASC == nullptr)
+	{
+		return;
+	}
+
+	check(gameplayEffectClass);
+	FGameplayEffectContextHandle effectContextHandle = targetASC->MakeEffectContext();
+	effectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle effectSpecHandle = targetASC->MakeOutgoingSpec(gameplayEffectClass, 1.f, effectContextHandle);
+	targetASC->ApplyGameplayEffectSpecToSelf(*effectSpecHandle.Data.Get());
 }
 
--- Source/ARPG_PROTO/Actor/ARPGEffectActor.h
+++ Source/ARPG_PROTO/Actor/ARPGEffectActor.h
@@ -4,7 +4,7 @@
 #include "GameFramework/Actor.h"
 #include "ARPGEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class ARPG_PROTO_API AARPGEffectActor : public AActor
@@ -15,18 +15,13 @@ public:
 	// Sets default values for this actor's properties
 	AARPGEffectActor();
 
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex, bool bFromSweep, const FHitResult& sweepResult);
-
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* overlappedComponent, AActor* otherActor, UPrimitiveComponent* otherComp, int32 otherBodyIndex);
 protected:
 	virtual void BeginPlay() override;
+
 private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> sphere;
-
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> mesh;
-
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* target, TSubclassOf<UGameplayEffect> gameplayEffectClass);
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> instantGameplayEffectClass;
 };
